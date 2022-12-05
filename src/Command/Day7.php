141401968606477da0a27ae3bebb3b883dc07032
Marijn Doeve <marijn@doeve.me>
2022-12-05 21:06:21 +0100

    Prep Day 7

---
+++ src/Command/Day7.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'aoc:7')]
+class Day7 extends AocCommand
+{
+    /** @param string[] $input */
+    protected function partOne(array $input, OutputInterface $output): void
+    {
+    }
+
+    /** @param string[] $input */
+    protected function partTwo(array $input, OutputInterface $output): void
+    {
+    }
+}
