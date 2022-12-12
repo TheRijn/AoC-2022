581e5d2ac6dab189132322d5c5b15eefe1937bf3
Marijn Doeve <marijn@doeve.me>
2022-12-12 20:41:55 +0100

    Day 12 part one

--- src/Command/Day12.php
+++ src/Command/Day12.php
@@ -4,57 +4,155 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use Ds\PriorityQueue;
 use Ds\Vector;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'aoc:12')]
+/**
+ * @phpstan-type Position array{int, int}
+ */
 class Day12 extends AocCommand
 {
-    /** @var array<array<string>> */
+    /** @var array<int[]> */
     private array $grid = [];
-    /** @var array<array<int>> */
-    private array $distance = [];
+    /** @var array<int[]> */
+    private array $distances = [];
 
-    /** @var array{int, int} */
-    private array $currentPos;
+    /** @phpstan-var Position */
+    private $start;
 
-    private function readGrid(Vector $input): void
+    /** @phpstan-var Position */
+    private $end;
+
+    /** @phpstan-var PriorityQueue<array{int, Position}> $queue */
+    private PriorityQueue $queue;
+
+    private int $height;
+    private int $width;
+
+    /** @param Vector<string> $input */
+    private function readAndDoTheDijkstra(Vector $input): int
     {
         // Read grid
-        foreach ($input as $line) {
-            $this->grid[] = str_split($line);
+        foreach ($input as $y => $line) {
+            $row = [];
+            foreach (str_split($line) as $x => $cell) {
+                if ($cell === 'S') {
+                    $this->start = [$y, $x];
+                    $value = 0;
+                } elseif ($cell === 'E') {
+                    $this->end = [$y, $x];
+                    $value = 25;
+                } else {
+                    $value = ord($cell) - ord('a');
+                }
+
+                $row[] = $value;
+            }
+            $this->grid[] = $row;
         }
+
+        $this->height = count($this->grid);
+        $this->width = count($this->grid[0]);
 
         // Set distances to inf
         foreach ($this->grid as $row) {
-            $this->distance[] = array_map(static fn() => PHP_INT_MAX, $row);
+            $this->distances[] = array_map(static fn() => PHP_INT_MAX, $row);
         }
-        // Find start
-        foreach ($this->grid as $y => $row) {
-            foreach ($row as $x => $height) {
-                if ($height === 'S') {
-                    $this->currentPos = [$y, $x];
-                    $this->distance[$y][$x] = 0;
-                    break;
+
+        $this->queue = new PriorityQueue();
+
+
+        $this->queue->push([0, $this->start], 0);
+        $this->setDistance($this->start, 0);
+
+        while (!$this->queue->isEmpty()) {
+            [$distU, $u] = $this->queue->pop();
+
+            if ($this->getDistance($u) < $distU) {
+                continue;
+            }
+
+            foreach ($this->getNeighbors($u) as $v) {
+                if ($this->getDistance($v) > $this->getDistance($u) + 1) {
+                    $this->setDistance($v, $this->getDistance($u) + 1);
+                    $this->addToQueue($v, $this->getDistance($v));
                 }
             }
         }
+
+        return $this->getDistance($this->end);
     }
 
-    private function getNeighbors(int $y, int $x): array
+    /** @phpstan-param Position $position */
+    private function addToQueue($position, int $distance): void
     {
+        $prioDistance = -$distance;
+
+        $this->queue->push([$distance, $position], $prioDistance);
     }
 
-    private function doTheDijkstra()
+    /** @phpstan-param Position $position */
+    private function getHeight($position): int
     {
+        return $this->grid[$position[0]][$position[1]];
     }
+
+    /** @phpstan-param Position $position */
+    private function getDistance($position): int
+    {
+        return $this->distances[$position[0]][$position[1]];
+    }
+
+    /** @phpstan-param Position $position */
+    private function setDistance($position, int $distance): void
+    {
+        $this->distances[$position[0]][$position[1]] = $distance;
+    }
+
+    /**
+     * @phpstan-param Position $position
+     *
+     * @phpstan-return Position[]
+     */
+    public function getNeighbors($position): array
+    {
+        $neighbors = [];
+
+        $possibleNeighbors = [];
+
+        foreach (range($position[0] - 1, $position[0] + 1) as $y) {
+            foreach (range($position[1] - 1, $position[1] + 1) as $x) {
+                if (
+                    ((abs($position[0] - $y) >= 1) && (abs($position[1] - $x) >= 1)) ||
+                    $y < 0 || $x < 0 || $y >= $this->height || $x >= $this->width ||
+                    ($y === $position[0] && $x === $position[1])
+                ) {
+                    continue;
+                }
+                $possibleNeighbors[] = [$y, $x];
+            }
+        }
+
+        $height = $this->getHeight($position);
+
+        foreach ($possibleNeighbors as $neighbor) {
+            $newHeight = $this->getHeight($neighbor);
+            if ($newHeight <= $height + 1) {
+                $neighbors[] = $neighbor;
+            }
+        }
+
+        return $neighbors;
+    }
+
 
     /** @param Vector<string> $input */
     protected function partOne(Vector $input, OutputInterface $output): void
     {
-        $this->readGrid($input);
-        $this->doTheDijkstra();
+        $output->writeln((string)$this->readAndDoTheDijkstra($input));
     }
 
     /** @param Vector<string> $input */
