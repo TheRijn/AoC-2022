0d6672102b1469ae0624a3bb4c501647d4a4f3a3
Marijn Doeve <marijn@doeve.me>
2022-12-15 22:49:47 +0100

    Refactor before 15 part 2

--- src/Command/Day15.php
+++ src/Command/Day15.php
@@ -7,6 +7,7 @@ namespace App\Command;
 use Ds\Vector;
 use Safe\Exceptions\PcreException;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'aoc:15')]
@@ -16,9 +17,13 @@ class Day15 extends AocCommand
     "closest beacon is at x=(?<x_beacon>-?\d+), y=(?<y_beacon>-?\d+)/";
 
     private int $checkY = 2_000_000;
+    private int $maxXY = 4_000_000;
 
-    /** @var array<array{sensor: array{int, int}, beacon: array{int, int}}> $sensors */
+    /** @var array<array{array{int, int}, int}> $sensors */
     private array $sensors;
+
+    /** @var array<array{int, int}> $beacons */
+    private array $beacons;
 
     /** @param Vector<string> $input
      * @throws PcreException
@@ -27,7 +32,6 @@ class Day15 extends AocCommand
     {
         $this->readCoordinates($input);
         $line = $this->drawLine($this->checkY);
-        ksort($line);
         $output->writeln((string)count($line));
     }
 
@@ -38,16 +42,21 @@ class Day15 extends AocCommand
     private function readCoordinates(Vector $input): void
     {
         $this->sensors = [];
+
         foreach ($input as $line) {
             \Safe\preg_match(self::RE, $line, $matches);
-            $this->sensors[] = [
-                "sensor" => [(int)$matches["x_sensor"], (int)$matches["y_sensor"]],
-                "beacon" => [(int)$matches["x_beacon"], (int)$matches["y_beacon"]],
-            ];
+            $sensor = [(int)$matches["x_sensor"], (int)$matches["y_sensor"]];
+            $beacon = [(int)$matches["x_beacon"], (int)$matches["y_beacon"]];
+
+            $dist = self::manhattanDist($sensor, $beacon);
+
+            $this->sensors[] = [$sensor, $dist];
+            $this->beacons[] = $beacon;
         }
 
         if (count($this->sensors) <= 14) {
             $this->checkY = 10;
+            $this->maxXY = 20;
         }
     }
 
@@ -56,44 +65,58 @@ class Day15 extends AocCommand
     {
         $line = [];
 
-        foreach ($this->sensors as $sensorSet) {
-            $sensor = $sensorSet['sensor'];
-            $dist = self::manhattanDist($sensorSet);
-
+        foreach ($this->sensors as [$sensor, $dist]) {
             // draw left
             $x = $sensor[0];
-            while (self::manhattanDist(['sensor' => $sensor, 'beacon' => [$x, $y]]) <= $dist) {
+            while (self::manhattanDist($sensor, [$x, $y]) <= $dist) {
                 $line[$x--] = 1;
             }
 
             // draw right
             $x = $sensor[0] + 1;
-            while (self::manhattanDist(['sensor' => $sensor, 'beacon' => [$x, $y]]) <= $dist) {
+            while (self::manhattanDist($sensor, [$x, $y]) <= $dist) {
                 $line[$x++] = 1;
             }
         }
 
         // remove beacons and sensors
-        foreach ($this->sensors as $sensorSet) {
-            foreach ($sensorSet as $thingy) {
-                if ($thingy[1] === $this->checkY) {
-                    unset($line[$thingy[0]]);
-                }
+        foreach ($this->sensors as [$sensor]) {
+            if ($sensor[1] === $this->checkY) {
+                unset($line[$sensor[0]]);
+            }
+        }
+        foreach ($this->beacons as $beacon) {
+            if ($beacon[1] === $this->checkY) {
+                unset($line[$beacon[0]]);
             }
         }
 
         return $line;
     }
 
-    /** @param array{sensor: array{int, int}, beacon: array{int, int}} $coordinates */
-    private static function manhattanDist(array $coordinates): int
+
+    /**
+     * @param array{int, int} $first
+     * @param array{int, int} $second
+     */
+    private static function manhattanDist(array $first, array $second): int
     {
-        return abs($coordinates["sensor"][0] - $coordinates["beacon"][0]) +
-            abs($coordinates["sensor"][1] - $coordinates["beacon"][1]);
+        return abs($first[0] - $second[0]) +
+            abs($first[1] - $second[1]);
     }
 
-    /** @param Vector<string> $input */
+    /**
+     * @param Vector<string> $input
+     * @throws PcreException
+     */
     protected function partTwo(Vector $input, OutputInterface $output): void
     {
+        $this->readCoordinates($input);
+
+        $progressBar = new ProgressBar($output);
+        $progressBar->setFormat('very_verbose');
+
+
+        $output->writeln((string)($x * 4_000_000 + $y));
     }
 }
